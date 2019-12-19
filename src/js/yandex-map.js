1b04b15cf36fa5eaafe466be26a7785794c38c4c
SergSmith64 <sergsmith2008@yandex.ru>
2019-12-19 17:04:40 +0400

    feat: webpack is work but without pics

--- src/js/yandex-map.js
+++
@@ -1,9 +0,0 @@
-// Функция ymaps.ready() будет вызвана, когда
-// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-ymaps.ready(init);
-function init(){
-  var myMap = new ymaps.Map("map", {
-    center: [51.544248, 46.049506],
-    zoom: 11
-  });
-}
